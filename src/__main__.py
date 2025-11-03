455c83169bafaf0086168fad610ceed4cab3d70e
shuvo881 <golammostofa10001@gmail.com>
2025-11-03 16:25:14 +0600

    fast api initalized and uv package manager setup

---
+++ src/__main__.py
@@ -0,0 +1,10 @@
+import uvicorn
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "app:app",           # points to the FastAPI instance in main.py
+        host="0.0.0.0",       # accessible on all network interfaces
+        port=8000,            # port number
+        reload=True,          # auto-reload on code changes
+        log_level="info"      # can be "debug", "info", "warning", etc.
+    )
